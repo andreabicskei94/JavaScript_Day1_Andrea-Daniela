8b17b5a551c9b4b0879eafdf957f50ac24841604
ajnacodes <b.daniela196@outlook.com>
2022-05-25 15:10:43 +0200

    a1 done

---
+++ Daniela/advanced/a1.js
@@ -0,0 +1,27 @@
+var a = 7;
+
+var b = 14;
+
+var c = "21";
+
+var d = '36';
+
+var e = 42;
+
+var c = Number(c)
+var d = Number(d)
+
+console.log(a + b + c + d + e);
+
+
+var f = '1';
+
+var g = 15;
+
+var h = 8;
+
+var i = "1";
+
+var f = Number(f)
+var i = Number(i)
+console.log(f * g * h * i);
