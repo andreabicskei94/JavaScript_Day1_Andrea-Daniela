07ba4cd524d567a2b132d4b7fda53037dd334317
Andrea Bicskei <andrea.bcsk@gmail.com>
2022-05-25 11:20:33 +0200

    int2_done

---
+++ intermediate_2/script.js
@@ -0,0 +1,12 @@
+var fruits = ['apple', 'banana', 'kiwi'];
+fruits.push('orange');
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+
+var animals = ['horse', 'monkey', 'dog'];
+animals.pop();
+animals.unshift('dog');
+console.log(animals);
+animals.unshift('cat');
+console.log(animals);
