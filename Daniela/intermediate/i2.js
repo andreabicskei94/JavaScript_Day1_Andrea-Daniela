f864604695264536304c62cf2d1d59417b97ddd9
ajnacodes <b.daniela196@outlook.com>
2022-05-25 14:40:16 +0200

    i2 done

--- Daniela/intermediate/i2.js
+++ Daniela/intermediate/i2.js
@@ -5,3 +5,11 @@ console.log(fruits);
 fruits.push("orange");
 
 console.log(fruits);
+
+const animals = ["dog", "horse", "monkey"];
+
+console.log(animals);
+
+animals.unshift("cat");
+
+console.log(animals);
