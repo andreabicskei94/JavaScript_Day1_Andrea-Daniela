df6fbf4c3ea277556cf42b82d373fb3f3fdf2d31
ajnacodes <b.daniela196@outlook.com>
2022-05-26 08:55:30 +0200

    challenge

---
+++ Daniela/challenge/challenge.js
@@ -0,0 +1,18 @@
+var long_string = "Properties,$a$set$of$immutable$values,$are$passed$to$a$component's$renderer$as$properties$in$its $HTML$tag.      $A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$properties $flow$down;$actions$flow$up"
+
+var long_string_array = long_string.split("$");
+
+console.table(long_string_array);
+
+long_string_array = long_string_array.join(" ");
+document.write("<br>");
+document.write("<br>");
+document.write(long_string_array);
+console.log(long_string_array);
+
+
+
+let text = "Properties,$a$set$of$immutable$values,$are$passed$to$a$component's$renderer$as$properties$in$its $HTML$tag.$A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$properties $flow$down;$actions$flow$up";
+let newText = text.replaceAll("$", " ");
+console.log(newText);
+document.write(newText);
